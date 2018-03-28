dd6cc42b8f2583d947a3918adfe21cc7bb2c2716
Fraillove <34437318+Fraillove@users.noreply.github.com>
2018-03-28 17:04:31 +0800

    Update ex6.sql

--- examples/ex6.sql
+++ examples/ex6.sql
@@ -26,34 +26,38 @@ This software has been released under the MIT license:
 */
 
 set serveroutput on format wrapped;
-declare
-  obj json;
-  obj2 json;
-  str varchar2(20);
-  pair_value json_value;
-  procedure p(v varchar2) as begin dbms_output.put_line(null);dbms_output.put_line(v); end;
-begin
-  p('Variables are copies');
-  obj := json(); --an empty structure
-  str := 'ABC';
-  obj.put('N1', str);
-  str := 'DEF';
-  obj.print;
-  obj.put('N2', str);
-  obj.print;
-  
-  p('Even nested json are copies');
-  obj2 := json('{"a":true}');
-  obj.put('N1', obj2);
-  obj2.remove('a');
-  obj.put('N2', obj2);
-  obj.print;
-  
-  p('Extract data from json');
-  pair_value := obj.get('N1');
-  --what to do with json_value? get the content into the right type!
-  obj2 := json(pair_value); --json construtor with json_value only works if json_value contains a json
-  obj2.print;
-  --JSON_LIST works in the same manner.
-end;
+DECLARE
+    obj        pljson;
+    obj2       pljson;
+    str        VARCHAR2(20);
+    pair_value pljson_value;
+    PROCEDURE p(v VARCHAR2) AS
+    BEGIN
+        dbms_output.put_line(NULL);
+        dbms_output.put_line(v);
+    END;
+BEGIN
+    p('Variables are copies');
+    obj := pljson(); --an empty structure
+    str := 'ABC';
+    obj.put('N1', str);
+    str := 'DEF';
+    obj.print;
+    obj.put('N2', str);
+    obj.print;
+
+    p('Even nested json are copies');
+    obj2 := pljson('{"a":true}');
+    obj.put('N1', obj2);
+    obj2.remove('a');
+    obj.put('N2', obj2);
+    obj.print;
+
+    p('Extract data from json');
+    pair_value := obj.get('N1');
+    --what to do with json_value? get the content into the right type!
+    obj2 := pljson(pair_value); --json construtor with json_value only works if json_value contains a json
+    obj2.print;
+    --JSON_LIST works in the same manner.
+END;
 /
