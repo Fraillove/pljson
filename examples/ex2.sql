b98435089a50b6f5080abeddb9f531c9cd5f9d71
Fraillove <34437318+Fraillove@users.noreply.github.com>
2018-03-28 16:36:01 +0800

    Update ex2.sql

--- examples/ex2.sql
+++ examples/ex2.sql
@@ -26,12 +26,12 @@ This software has been released under the MIT license:
 */
 
 set serveroutput on format wrapped;
-declare
-  obj json_list;
-begin
-  obj := json_list('[1,2,3,[3, []]]'); --empty list and nested lists are supported
-  obj.print;
-  dbms_output.put_line(obj.to_char); --equivalent to print
-  dbms_output.put_line(obj.to_char(false)); --equivalent to print(false) -- compact
-end;
+DECLARE
+    obj pljson_list;
+BEGIN
+    obj := pljson_list('[1,2,3,[3, []]]'); --empty list and nested lists are supported
+    obj.print;
+    dbms_output.put_line(obj.to_char); --equivalent to print
+    dbms_output.put_line(obj.to_char(FALSE)); --equivalent to print(false) -- compact
+END;
 /
