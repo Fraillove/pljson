01005a1c68409fdfc83a7f3fda029e51d6d56fc1
Fraillove <34437318+Fraillove@users.noreply.github.com>
2018-03-28 16:47:31 +0800

    Update ex3.sql

--- examples/ex3.sql
+++ examples/ex3.sql
@@ -26,23 +26,21 @@ This software has been released under the MIT license:
   The parser follows the json specification described @ www.json.org  
 */
 
-set serveroutput on format wrapped;
-declare
-  scanner_exception exception;
-  pragma exception_init(scanner_exception, -20100);
-  parser_exception exception;
-  pragma exception_init(parser_exception, -20101);
+DECLARE
+    scanner_exception EXCEPTION;
+    PRAGMA EXCEPTION_INIT(scanner_exception, -20100);
+    parser_exception EXCEPTION;
+    PRAGMA EXCEPTION_INIT(parser_exception, -20101);
 
-  obj json;
-begin
-  obj := json('this is not valid json'); 
-  --displays ORA-20100: JSON Scanner exception @ line: 1 column: 1 - Expected: 'true'
-  --thats because the closest match was a boolean
-  obj.print;
-exception 
-  when scanner_exception then
-    dbms_output.put_line(SQLERRM);
-  when parser_exception then
-    dbms_output.put_line(SQLERRM);
-end;
-/
+    obj pljson;
+BEGIN
+    obj := pljson('this is not valid json');
+    --displays ORA-20100: JSON Scanner exception @ line: 1 column: 1 - Expected: 'true'
+    --thats because the closest match was a boolean
+    obj.print;
+EXCEPTION
+    WHEN scanner_exception THEN
+        dbms_output.put_line(SQLERRM);
+    WHEN parser_exception THEN
+        dbms_output.put_line(SQLERRM);
+END;
