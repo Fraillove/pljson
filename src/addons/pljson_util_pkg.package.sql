6d6f7eb7384a3116849cd0fc46a89a19d11f0053
Fraillove <34437318+Fraillove@users.noreply.github.com>
2018-05-11 13:02:26 +0800

    Update pljson_util_pkg.package.sql

--- src/addons/pljson_util_pkg.package.sql
+++ src/addons/pljson_util_pkg.package.sql
@@ -2,20 +2,6 @@ set define off
 
 create or replace package pljson_util_pkg authid current_user as
 
-  /*
-
-  Purpose:    JSON utilities for PL/SQL
-  see http://ora-00001.blogspot.com/
-  
-  Remarks:
-  
-  Who     Date        Description
-  ------  ----------  -------------------------------------
-  MBR     30.01.2010  Created
-  JKR     01.05.2010  Edited to fit in PL/JSON
-  JKR     19.01.2011  Newest stylesheet + bugfix handling
-  
-  */
 
   -- generate JSON from REF Cursor
   function ref_cursor_to_json (p_ref_cursor in sys_refcursor,
