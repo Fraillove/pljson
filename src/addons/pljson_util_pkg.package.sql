f4f07b4c6248352b6b63afac3ef09cfb63318609
Fraillove <34437318+Fraillove@users.noreply.github.com>
2018-05-11 09:45:08 +0800

    Update pljson_util_pkg.package.sql

--- src/addons/pljson_util_pkg.package.sql
+++ src/addons/pljson_util_pkg.package.sql
@@ -37,17 +37,6 @@ create or replace package body pljson_util_pkg as
   parser_exception exception;
   pragma exception_init(parser_exception, -20101);
 
-  /*
-
-  Purpose:    JSON utilities for PL/SQL
-
-  Remarks:
-
-  Who     Date        Description
-  ------  ----------  -------------------------------------
-  MBR     30.01.2010  Created
-
-  */
 
 
   g_json_null_object             constant varchar2(20) := '{ }';
@@ -58,47 +47,13 @@ as
 begin
 
   /*
-
-  Purpose:    return XSLT stylesheet for XML to JSON transformation
-
-  Remarks:    see http://code.google.com/p/xml2json-xslt/
-
-  Who     Date        Description
-  ------  ----------  -------------------------------------
-  MBR     30.01.2010  Created
-  MBR     30.01.2010  Added fix for nulls
-
-  */
+  return XSLT stylesheet for XML to JSON transformation
+ */
 
 
   return q'^<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<!--
-  Copyright (c) 2006,2008 Doeke Zanstra
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without modification,
-  are permitted provided that the following conditions are met:
-
-  Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer. Redistributions in binary
-  form must reproduce the above copyright notice, this list of conditions and the
-  following disclaimer in the documentation and/or other materials provided with
-  the distribution.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-  THE POSSIBILITY OF SUCH DAMAGE.
--->
-
-  <xsl:output indent="no" omit-xml-declaration="yes" method="text" encoding="UTF-8" media-type="text/x-json"/>
+<xsl:output indent="no" omit-xml-declaration="yes" method="text" encoding="UTF-8" media-type="text/x-json"/>
         <xsl:strip-space elements="*"/>
   <!--contant-->
   <xsl:variable name="d">0123456789</xsl:variable>
@@ -265,17 +220,8 @@ as
 begin
 
   /*
-
-  Purpose:    generate JSON from REF Cursor
-
-  Remarks:
-
-  Who     Date        Description
-  ------  ----------  -------------------------------------
-  MBR     30.01.2010  Created
-  JKR     01.05.2010  Edited to fit in PL/JSON
-
-  */
+  generate JSON from REF Cursor
+*/
 
   l_ctx := dbms_xmlgen.newcontext (p_ref_cursor);
   
